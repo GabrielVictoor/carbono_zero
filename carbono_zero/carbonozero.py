d83478fe7d2b40dd1563b351296f2f28f649b8c0
Gabriel Camargo <72475614+GabrielVictoor@users.noreply.github.com>
2023-11-13 16:42:48 -0300

    Update carbonozero.py
    Refatoração da Calculadora de Emissões de Carbono
    
    - Adaptação dos parâmetros de entrada para medir emissões nos escopos específicos de combustível, eletricidade e transporte.
    -  Aviso adicionado sobre o uso de '.' para separar as casas decimais nas entradas.
    - A lógica de cálculo foi ajustada para incluir todas as fontes de emissões e calcular o total em quilogramas.
    - A fórmula para determinar a quantidade de árvores necessárias foi atualizada para 22 kg de CO2e por árvore.
    - A interface gráfica foi modificada para refletir as novas entradas e apresentar resultados mais abrangentes.
    - O texto de descrição inicial foi atualizado para orientar o usuário sobre as mudanças nos escopos de emissão.

--- carbono_zero/carbonozero.py
+++ carbono_zero/carbonozero.py
@@ -1,52 +1,61 @@
 import tkinter as tk
-from tkinter import PhotoImage  # Importar a classe PhotoImage do tkinter
-from PIL import Image, ImageTk  # Importar o módulo Image e a classe ImageTk do Pillow
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
 
-# Função para calcular o impacto e o plantio de árvores necessárias
 def calcular_impacto():
-    tCO2e = float(entrada_tCO2e.get())
+    combustivel = float(entrada_combustivel.get())
+    eletricidade = float(entrada_eletricidade.get())
+    transporte = float(entrada_transporte.get())
+    total_tCO2e = combustivel + eletricidade + transporte
     preco_credito = float(entrada_preco_credito.get())
-    compensacao = tCO2e * preco_credito
-    arvores_necessarias = tCO2e / 7  # 7 árvores por tonelada
-    resultado.config(text=f"Compensação: R${compensacao:.2f}\nÁrvores necessárias: {arvores_necessarias:.2f} (em até 20 anos)")
-    
-# interface
+    total_kg = total_tCO2e * 1000
+    arvores_necessarias = total_kg / 22 
+    compensacao = total_tCO2e * preco_credito
+    resultado.config(text=f"Compensação: R${compensacao:.2f}\nÁrvores necessárias: {arvores_necessarias:.2f} árvores")
+
 janela = tk.Tk()
 janela.title("Calculadora de Emissões de Carbono")
 
-# inputs
-lbl_tCO2e = tk.Label(janela, text="Emissões de Carbono (tCO2e):")
-# Usando o método grid para posicionar o widget
-# O parâmetro sticky é usado para fazer o widget se expandir na direção horizontal
-lbl_tCO2e.grid(row=0, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
-entrada_tCO2e = tk.Entry(janela)
-entrada_tCO2e.grid(row=1, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+descricao_inicial = tk.Label(janela, text="Insira abaixo a quantidade de dióxido de carbono (em tCO2e) emitidos nos seguintes escopos:")
+descricao_inicial.grid(row=0, column=0, columnspan=2, pady=5)
+
+descricao_atencao = tk.Label(janela, text="ATENÇÃO: Utilize '.' ao invés de ',' para separar as casas decimais.")
+descricao_atencao.grid(row=1, column=0, columnspan=2, pady=5)
+
+lbl_combustivel = tk.Label(janela, text="Combustível (tCO2e):")
+lbl_combustivel.grid(row=2, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+entrada_combustivel = tk.Entry(janela)
+entrada_combustivel.grid(row=3, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+
+lbl_eletricidade = tk.Label(janela, text="Eletricidade (tCO2e):")
+lbl_eletricidade.grid(row=4, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+entrada_eletricidade = tk.Entry(janela)
+entrada_eletricidade.grid(row=5, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+
+lbl_transporte = tk.Label(janela, text="Transporte (tCO2e):")
+lbl_transporte.grid(row=6, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+entrada_transporte = tk.Entry(janela)
+entrada_transporte.grid(row=7, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
 
 lbl_preco_credito = tk.Label(janela, text="Preço do Crédito de Carbono (R$/tCO2e):")
-lbl_preco_credito.grid(row=2, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+lbl_preco_credito.grid(row=8, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
 entrada_preco_credito = tk.Entry(janela)
-entrada_preco_credito.grid(row=3, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
+entrada_preco_credito.grid(row=9, column=0, sticky=tk.W+tk.E, padx=10, pady=5)
 
-# Button
 calcular_button = tk.Button(janela, text="Calcular", command=calcular_impacto)
-calcular_button.grid(row=4, column=0, pady=10)
+calcular_button.grid(row=10, column=0, pady=10)
 
-# resultado
 resultado = tk.Label(janela, text="")
-resultado.grid(row=5, column=0, pady=10)
+resultado.grid(row=11, column=0, pady=10)
 
-# Carregar a imagem da árvore
 img_arvore = Image.open("tree.png")
 img_arvore = img_arvore.resize((32, 32))
 img_arvore = ImageTk.PhotoImage(img_arvore)
 img_label = tk.Label(janela, image=img_arvore)
-img_label.grid(row=6, column=0, pady=10)
+img_label.grid(row=12, column=0, pady=10)
 
-# Configurando o redimensionamento
-for i in range(7):  # Porque temos 7 rows
-    # grid_rowconfigure com weight=1 faz com que a linha expanda proporcionalmente quando a janela é redimensionada
+for i in range(13): 
     janela.grid_rowconfigure(i, weight=1)
-# grid_columnconfigure com weight=1 faz com que a coluna expanda proporcionalmente quando a janela é redimensionada
 janela.grid_columnconfigure(0, weight=1)
 
 janela.mainloop()
